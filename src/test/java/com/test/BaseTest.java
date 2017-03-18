cd21da72ff4c216e6d88f91e77a2f3a3fdb847db
weipon <weipon@weipondeMacBook-Pro.local>
2017-03-18 14:59:03 +0800

    changes log

--- src/test/java/com/test/BaseTest.java
+++ src/test/java/com/test/BaseTest.java
@@ -1,4 +1,4 @@
-package com.cassandra.test;
+package com.test;
 
 
 import org.junit.After;
---
+++ src/test/java/com/test/RepositoryTest.java
@@ -0,0 +1,25 @@
+package com.test;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.kafka.core.KafkaTemplate;
+
+
+public class RepositoryTest extends BaseTest {
+    private static final String TEMPLATE_TOPIC = "templateTopic";
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Autowired
+    private KafkaTemplate<Integer, String> kafkaTemplate;
+
+    @Test
+    public void Test() {
+
+
+    }
+
+
+}
